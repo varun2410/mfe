e839cafb2cc9de0ae8b5e4f59ccb0ea1dc097e33
Varun Agrawal <varun.agrawal@powerschool.com>
2023-01-04 08:19:23 +0530

    Dummy commit to trigger deployment

--- packages/container/src/bootstrap.js
+++ packages/container/src/bootstrap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+// This will render in the actual dom
 ReactDOM.render(
   <App />,
   document.querySelector('#root'),
